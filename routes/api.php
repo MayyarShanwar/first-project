bdc97476751d1b452a4520ef8bb3f9288c115671
Mayyar Shanwar <shanwar.mayyar@gmail.com>
2025-12-30 16:49:45 +0300

    remove some comments

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/posts', PostController::class);
     Route::apiResource('/categories', CategoryController::class);
     Route::apiResource('/authors', AuthorController::class);
     Route::apiResource('/comments', CommentController::class);
-// });
+});
 
 Route::post('/login', [AuthController::class, 'login']);
